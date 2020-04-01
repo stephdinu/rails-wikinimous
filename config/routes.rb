50045ae45057649434877b425c33c2b2dd174747
Stephanie Dinu <stephaniedinu@gmail.com>
2020-04-01 21:51:12 +0200

    Wikinominous completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "articles", to: "articles#index"
+  get "articles", to: "articles#show", as: :article
+  get "articles", to: "articles#new"
+  post "articles", to: "articles#create"
+  get "articles/:id/edit", to: "article#edit"
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
 end
